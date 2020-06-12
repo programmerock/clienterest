5f5c424721d39952d5fc5c7406241411ef106f2a
programmerock <framoron8@gmail.com>
2020-06-12 02:27:21 -0300

    2

---
+++ src/ar/org/centro8/curso/java/aplicaciones/dao/rest/cliente/ClienteRepository.java
@@ -0,0 +1,92 @@
+package ar.org.centro8.curso.java.aplicaciones.dao.rest.cliente;
+
+import ar.org.centro8.curso.java.aplicaciones.dao.interfaces.I_ClienteRepository;
+import ar.org.centro8.curso.java.aplicaciones.entities.Cliente;
+import ar.org.centro8.curso.java.aplicaciones.enumerados.TipoDocumento;
+import java.util.List;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+public class ClienteRepository implements I_ClienteRepository {
+
+    public String uri;
+
+    public ClienteRepository(String uri) {
+        this.uri = uri;
+    }
+
+    @Override
+    public void save(Cliente cliente) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(uri + "alta?nombre=" + cliente.getNombre()
+                            + "&apellido=" + cliente.getApellido()
+                            + "&tipoDocumento=" + cliente.getTipoDocumento()
+                            + "&numeroDocumento=" + cliente.getNumeroDocumento()
+                            + "&direccion=" + cliente.getDireccion()
+                            + "&comentarios=" + cliente.getComentarios()))
+                    .build();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            cliente.setId(Integer.parseInt(response.body()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(Cliente cliente) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(uri + "baja?id=" + cliente.getId()))
+                    .build();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Cliente> getAll() {
+        List<Cliente>list=new ArrayList();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(uri + "all"))
+                    .build();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            String resp = response.body();                
+            String[] lines=resp.split("Cliente");
+            for(String l:lines){
+                int longitud=l.length();
+                if(longitud>0){
+                l=l.substring(1,longitud -2);
+                String[] campos=l.split(", ");
+                list.add(new Cliente(
+                        Integer.parseInt(campos[0].substring(3)),
+                        campos[1].substring(7),
+                        campos[2].substring(9),
+                        TipoDocumento.valueOf(campos[3].substring(14)),
+                        Integer.parseInt(campos[4].substring(16)),
+                        campos[5].substring(10),
+                        campos[6].substring(12)                        
+                ));
+            }
+            }
+        } catch (Exception e) {e.printStackTrace(); }
+        return list;
+    }
+
+}
---
+++ src/ar/org/centro8/curso/java/aplicaciones/test/TestClienteRepository.java
@@ -0,0 +1,50 @@
+package ar.org.centro8.curso.java.aplicaciones.test;
+
+import ar.org.centro8.curso.java.aplicaciones.dao.interfaces.I_ClienteRepository;
+import ar.org.centro8.curso.java.aplicaciones.dao.rest.cliente.ClienteRepository;
+import ar.org.centro8.curso.java.aplicaciones.entities.Cliente;
+import ar.org.centro8.curso.java.aplicaciones.enumerados.TipoDocumento;
+
+public class TestClienteRepository {
+    public static void main(String[] args) {
+        I_ClienteRepository cr=new ClienteRepository ("http://localhost:8081/Server/clientes/v1/");
+        System.out.println("Inicio del TestClienteRepository\n");
+        System.out.println("****************************\n");
+        
+        System.out.println("Ingreso 2 clientes (Marcelo Ramirez y Carlos Paez)con el método 'save'");
+        Cliente cliente=new Cliente("Mario", "Ramos", TipoDocumento.DNI, 37412822, "Brown_354", null);
+        cr.save(cliente);
+        Cliente cliente2= new Cliente("Claudio", "Parrili", TipoDocumento.DNI, 12349922, "Belgrano_165","cliente_corporativo");
+        cr.save(cliente2);
+        System.out.println(cliente+"\n"+cliente2);
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Recorro la lista de clientes con un forEach del método 'getAll'");
+        cr.getAll().forEach(System.out::println);
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Busco el registro de id:2 con el método 'getById'");
+        System.out.println(cr.getById(2));
+        System.out.println("\n****************************\n");
+                    
+        System.out.println("Busco el registro con DNI 12345678 con el método 'getByTipoNumero'");
+        System.out.println(cr.getByTipoNumero(TipoDocumento.DNI, 12345678));
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Busco los registros que contengan la letra 'z' en su apellido con el método 'getLikeApellido'");
+        cr.getLikeApellido("z").forEach(System.out::println);
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Busco el registro de nombre y apellido 'Marcelo Ramirez' con el método 'getLikeApellidoNombre'");
+        cr.getLikeApellidoNombre("Ramirez", "Marcelo").forEach(System.out::println);
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Elimino el registro de id:3 con el método 'remove'");
+        cr.remove(cr.getById(13));
+        System.out.println("Recorro la lista para ver que ya no figure:");
+        cr.getAll().forEach(System.out::println);
+        System.out.println("\n****************************\n");
+        
+        System.out.println("Fin del TestClienteRepository");        
+}
+}
